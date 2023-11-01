e4e2bb6923a1c5b3ca6cad878cd064f4b246228b
soluxum32 <142458264+soluxum32@users.noreply.github.com>
2023-11-01 18:05:12 -0400

    Update cipher.py

--- cipher.py
+++ cipher.py
@@ -6,9 +6,12 @@ def caesar_cipher(text, shift):
             is_upper = char.isupper()
             char_offset = ord('A' if is_upper else 'a')
             encrypted_char = chr((ord(char) - char_offset + shift) % 26 + char_offset)
+            if is_upper:
+                encrypted_text += encrypted_char.upper()
+            else:
+                encrypted_text += encrypted_char
         else:
-            encrypted_char = char
-        encrypted_text += encrypted_char
+            encrypted_text += char
     return encrypted_text
 
 plain_text = input("Please enter a sentence: ")
@@ -16,4 +19,3 @@ shift = 5
 encrypted_text = caesar_cipher(plain_text, shift)
 
 print("The encrypted sentence is:", encrypted_text)
-
