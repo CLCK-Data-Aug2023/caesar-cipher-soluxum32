e79881e1419b945b0477bc545505131f0078039b
soluxum32 <142458264+soluxum32@users.noreply.github.com>
2023-11-01 18:37:48 -0400

    Update cipher.py

--- cipher.py
+++ cipher.py
@@ -2,17 +2,23 @@
 def caesar_cipher(text, shift):
     encrypted_text = ""
     for char in text:
-        if char.isalpha():
-            is_upper = char.isupper()
-            char_offset = ord('A' if is_upper else 'a')
-            encrypted_char = chr(((ord(char) - char_offset + shift) % 26) + char_offset)
+        if char.isalpha():  # Check if the character is an alphabet letter
+            is_upper = char.isupper()  # Check if the character is uppercase
+            char = char.lower()  # Convert to lowercase for easy shifting
+            shifted = chr(((ord(char) - ord('a') + shift) % 26) + ord('a'))
+            if is_upper:
+                shifted = shifted.upper()  # Restore the case if it was uppercase
+            encrypted_text += shifted
         else:
-            encrypted_char = char
-        encrypted_text += encrypted_char
+            encrypted_text += char  # Special characters remain unchanged
     return encrypted_text
 
-plain_text = input("Please enter a sentence: ")
-shift = 5
-encrypted_text = caesar_cipher(plain_text, shift)
+def main():
+    plain_text = input("Enter a sentence to encrypt: ")
+    shift = 5  # Right shift by 5 positions
 
-print("The encrypted sentence is:", encrypted_text)
+    encrypted_text = caesar_cipher(plain_text, shift)
+    print("Encrypted text: " + encrypted_text)
+
+if __name__ == "__main":
+    main()
