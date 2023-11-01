2267acfa3f3d92a6e7b1e8ba897bfe3ae262c455
soluxum32 <142458264+soluxum32@users.noreply.github.com>
2023-11-01 18:25:33 -0400

    Update cipher.py

--- cipher.py
+++ cipher.py
@@ -1,26 +1,18 @@
 # add your code here
-def caesar_cipher_encrypt(text, shift):
+def caesar_cipher(text, shift):
     encrypted_text = ""
     for char in text:
         if char.isalpha():
-            # Determine whether the character is uppercase or lowercase
-            if char.isupper():
-                char_offset = ord('A')
-            else:
-                char_offset = ord('a')
-            # Apply the Caesar cipher shift
-            shifted_char = chr((ord(char) - char_offset + shift) % 26 + char_offset)
-            encrypted_text += shifted_char
+            is_upper = char.isupper()
+            char_offset = ord('A' if is_upper else 'a')
+            encrypted_char = chr((ord(char) - char_offset + shift) % 26 + char_offset)
         else:
-            # Keep special characters unchanged
-            encrypted_text += char
+            encrypted_char = char
+            encrypted_text += encrypted_char
     return encrypted_text
 
-def main():
-    plain_text = input("Please enter a sentence: ")
-    shift = 5
-    encrypted_text = caesar_cipher_encrypt(plain_text, shift)
-    print("The encrypted sentence is:", encrypted_text)
+plain_text = input("Please enter a sentence: ")
+shift = 5
+encrypted_text = caesar_cipher(plain_text, shift)
 
-if __name__ == "__main":
-    main()
+print("The encrypted sentence is:", encrypted_text)
