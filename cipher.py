cb244922d9563e39667ff684bfba074a7125c3ca
soluxum32 <142458264+soluxum32@users.noreply.github.com>
2023-11-01 19:55:55 -0400

    Update cipher.py

--- cipher.py
+++ cipher.py
@@ -4,11 +4,12 @@ def caesar_cipher(text, shift):
     encrypted_text = ""
     for char in text:
         if char.isalpha():
+            is_upper = char.isupper()
             shift_amount = shift % 26
-            if char.islower():
-                encrypted_char = chr(((ord(char) - ord('a') + shift_amount) % 26) + ord('a'))
-            else:
-                encrypted_char = chr(((ord(char) - ord('A') + shift_amount) % 26) + ord('A'))
+            char = char.lower()
+            encrypted_char = chr(((ord(char) - ord('a') + shift_amount) % 26) + ord('a'))
+            if is_upper:
+                encrypted_char = encrypted_char.upper()
         else:
             encrypted_char = char
         encrypted_text += encrypted_char
@@ -17,3 +18,5 @@ def caesar_cipher(text, shift):
 plain_text = input("Please enter a sentence: ")
 encrypted_text = caesar_cipher(plain_text, 5)
 print("The encrypted sentence is:", encrypted_text)
+
+
