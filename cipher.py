da3f41c56263f88faecafe870807694da5871285
soluxum32 <142458264+soluxum32@users.noreply.github.com>
2023-11-01 17:47:58 -0400

    Update cipher.py

--- cipher.py
+++ cipher.py
@@ -3,12 +3,10 @@ def caesar_cipher(text, shift):
     encrypted_text = ""
     for char in text:
         if char.isalpha():
-            # Determine if the character is uppercase or lowercase
             is_upper = char.isupper()
-            char_offset = ord('A') if is_upper else ord('a')
+            char_offset = ord('A' if is_upper else 'a')
             encrypted_char = chr((ord(char) - char_offset + shift) % 26 + char_offset)
         else:
-            # Keep special characters as they are
             encrypted_char = char
         encrypted_text += encrypted_char
     return encrypted_text
