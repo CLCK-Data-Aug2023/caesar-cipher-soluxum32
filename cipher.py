d55f23232023cd99fc7e98807ab8315e63b00b9f
soluxum32 <142458264+soluxum32@users.noreply.github.com>
2023-11-01 19:30:49 -0400

    Update cipher.py

--- cipher.py
+++ cipher.py
@@ -1,26 +1,23 @@
 # add your code here
 
-def caesar_decrypt(encrypted_text, shift):
-    decrypted_text = ""
-    for char in encrypted_text:
+def caesar_cipher(text, shift):
+    encrypted_text = ""
+    for char in text:
         if char.isalpha():
             is_upper = char.isupper()
             char_offset = ord('A' if is_upper else 'a')
-            char = char.lower()  # Convert the character to lowercase for comparison
-            decrypted_char = chr(((ord(char) - char_offset - shift) % 26) + char_offset)
+            encrypted_char = chr(((ord(char) - char_offset + shift) % 26) + char_offset)
             if is_upper:
-                decrypted_text += decrypted_char.upper()
+                encrypted_text += encrypted_char.upper()
             else:
-                decrypted_text += decrypted_char
+                encrypted_text += encrypted_char
         else:
-            decrypted_text += char
-    return decrypted_text
+            encrypted_text += char
+    return encrypted_text
 
-encrypted_sentence = 'f xjsyjshj bnym hfunyfq qjyyjwx.'
+plain_text = input("Please enter a sentence: ")
 shift = 5
-decrypted_sentence = caesar_decrypt(encrypted_sentence, shift)
+encrypted_text = caesar_cipher(plain_text, shift)
 
-print("The decrypted sentence is:", decrypted_sentence)
-
-
+print("The encrypted sentence is:", encrypted_text)
 
