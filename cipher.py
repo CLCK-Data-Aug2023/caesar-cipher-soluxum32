14ee73d483f4d6a632063b36ec691d10a1214a36
soluxum32 <142458264+soluxum32@users.noreply.github.com>
2023-11-01 19:04:52 -0400

    Update cipher.py

--- cipher.py
+++ cipher.py
@@ -1,22 +1,20 @@
 # add your code here
 
 def caesar_cipher(text, shift):
-    encrypted_text = ""
+    result = ""
     for char in text:
         if char.isalpha():
-            is_upper = char.isupper()
-            char_offset = ord('A' if is_upper else 'a')
-            encrypted_char = chr(((ord(char) - char_offset + shift) % 26) + char_offset)
-            if is_upper:
-                encrypted_text += encrypted_char.upper()
+            if char.islower():
+                result += chr(((ord(char) - ord('a') + shift) % 26) + ord('a'))
             else:
-                encrypted_text += encrypted_char
+                result += chr(((ord(char) - ord('A') + shift) % 26) + ord('A'))
         else:
-            encrypted_text += char
-    return encrypted_text
+            result += char
+    return result
 
-plain_text = input("Please enter a sentence: ")
-shift = 5
-encrypted_text = caesar_cipher(plain_text, shift)
+text = "A sentence with Capital letters."
+shift = 5  # You can change the shift value according to your needs
 
-print("The encrypted sentence is:", encrypted_text)
+ciphered_text = caesar_cipher(text, shift)
+print("Ciphered text:", ciphered_text)
+
