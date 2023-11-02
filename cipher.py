b1c11c957f37f0732053e51966c5fbc2736ac443
soluxum32 <142458264+soluxum32@users.noreply.github.com>
2023-11-01 20:02:17 -0400

    Update cipher.py

--- cipher.py
+++ cipher.py
@@ -1,22 +1,27 @@
 # add your code here
 
-def caesar_cipher(text, shift):
-    encrypted_text = ""
-    for char in text:
-        if char.isalpha():
-            is_upper = char.isupper()
-            shift_amount = shift % 26
-            char = char.lower()
-            encrypted_char = chr(((ord(char) - ord('a') + shift_amount) % 26) + ord('a'))
-            if is_upper:
-                encrypted_char = encrypted_char.upper()
-        else:
-            encrypted_char = char
-        encrypted_text += encrypted_char
-    return encrypted_text
+def encrypt(text,s):
+	result = ""
 
-plain_text = input("Please enter a sentence: ")
-encrypted_text = caesar_cipher(plain_text, 5)
-print("The encrypted sentence is:", encrypted_text)
+	# traverse text
+	for i in range(len(text)):
+		char = text[i]
+
+		# Encrypt uppercase characters
+		if (char.isupper()):
+			result += chr((ord(char) + s-65) % 26 + 65)
+
+		# Encrypt lowercase characters
+		else:
+			result += chr((ord(char) + s - 97) % 26 + 97)
+
+	return result
+
+#check the above function
+text = "WILDCATS"
+s = 5
+print ("Text : " + text)
+print ("Shift : " + str(s))
+print ("Cipher: " + encrypt(text,s))
 
 
